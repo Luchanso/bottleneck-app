b06d83853937625c8a7184c7c4b24cffb6ff7723
Luchanso <luchansodev@gmail.com>
2017-07-11 01:50:50 +0300

    add some prototype

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { CardActions, CardContent } from 'material-ui/Card';
-import { Typography, Card } from 'material-ui';
+import { shape, string } from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import { red, white } from 'material-ui/colors';
+import DeleteIcon from 'material-ui-icons/Delete';
+import PlayIcon from 'material-ui-icons/PlayArrow';
+import RestoreIcon from 'material-ui-icons/Restore';
+import { CardContent, CardActions } from 'material-ui/Card';
+import { Typography, TextField, Card, IconButton, Button } from 'material-ui';
 
-export default class Timer extends React.Component {
+class Timer extends React.Component {
   constructor(props) {
     super(props);
 
@@ -10,16 +16,73 @@ export default class Timer extends React.Component {
   }
 
   render() {
-    // const { classes } = props;
+    const { classes } = this.props;
 
     return (
       <Card>
         <CardContent>
-          <Typography type="body1">
+          <TextField
+            color="secondary"
+            id="name"
+            label="Name"
+            fullWidth
+          >
             Word of the Day
+          </TextField>
+          <Typography
+            className={classes.time}
+            align="center"
+            type="display1"
+          >
+            000:00:00
           </Typography>
+          <div className={classes.controls}>
+            <Button fab color="primary" className={classes.btnToggle}>
+              <PlayIcon />
+            </Button>
+          </div>
         </CardContent>
+        <CardActions disableActionSpacing className={classes.cardActions}>
+          <IconButton
+            className={classes.btnReset}
+            aria-label="Reset"
+          >
+            <RestoreIcon />
+          </IconButton>
+          <IconButton
+            className={classes.btnDelete}
+            aria-label="Delete"
+          >
+            <DeleteIcon />
+          </IconButton>
+        </CardActions>
       </Card>
-    )
+    );
   }
 }
+
+Timer.propTypes = {
+  classes: shape({
+    time: string.isRequired,
+  }).isRequired,
+};
+
+const styleSheet = createStyleSheet('Timers', theme => ({
+  time: {
+    marginTop: 16,
+    color: theme.palette.primary[500],
+  },
+  controls: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
+  btnToggle: {
+    color: 'white',
+  },
+  cardActions: {
+    justifyContent: 'flex-end',
+  },
+}));
+
+export default withStyles(styleSheet)(Timer);
