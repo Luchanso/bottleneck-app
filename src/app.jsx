1e1e45537e70bc3d7832776021a1bba067bd9e57
Luchanso <luchansodev@gmail.com>
2017-07-11 01:50:43 +0300

    add theming

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { MuiThemeProvider } from 'material-ui/styles';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { red, orange, indigo } from 'material-ui/colors';
+import createPalette from 'material-ui/styles/palette';
 import store from 'store';
 import Router from 'containers/Router';
 
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary: orange,
+    accent: indigo,
+    error: red,
+  }),
+});
+
 const App = () => (
   <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
       <Router />
     </MuiThemeProvider>
   </Provider>
